7945177c40f0e2b835c8887a9199950704b4f2e1
Justin <jforlenza@steamcenter.nyc>
2025-06-09 12:47:18 -0400

    added buying code for lune and maelle

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-let points = 1000
+let points = 0
 
 let gustaveLevel = 0
 let gLevel = document.getElementById('gLevel')
+
+let luneLevel = 0
+let lLevel = document.getElementById('lLevel')
+
+let maelleLevel = 0
+let mLevel = document.getElementById('mLevel')
 
 let pointCounter = document.getElementById("pointCounter")
 
@@ -19,6 +25,26 @@ function buyGustave() {
         changePoint(-100)
         gustaveLevel = gustaveLevel + 1
         gLevel.innerText = 'Level ' + gustaveLevel
+    } else {
+        alert("You poor")
+    }
+}
+
+function buyMaelle() {
+    if (points >= 10000) {
+        changePoint(-10000)
+        maelleLevel = maelleLevel + 1
+        mLevel.innerText = 'Level ' + maelleLevel
+    } else {
+        alert("You poor")
+    }
+}
+
+function buyLune() {
+    if (points >= 10000000) {
+        changePoint(-10000000)
+        luneLevel = luneLevel + 1
+        lLevel.innerText = 'Level ' + luneLevel
     } else {
         alert("You poor")
     }
